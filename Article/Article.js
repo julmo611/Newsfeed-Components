c8562e5fde3543f74b54cd6573dcada4f39c033e
Julian Moreno <julmo611@gmail.com>
2019-01-30 15:51:37 -0800

    MVP Done

--- Article/Article.js
+++ Article/Article.js
@@ -1,54 +1,56 @@
-// // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-// class Article {
-//   constructor(domElement) {
-//     // assign this.domElement to the passed in domElement
-//     this.domElement;
-//     // create a reference to the ".expandButton" class. 
-//     this.expandButton;
-//     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-//     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-//   }
+class Article {
+    constructor(domElement) {
+      // assign this.domElement to the passed in domElement
+      this.domElement = domElement;
+  
+      // create a reference to the ".expandButton" class.
+      this.expandButton = domElement.querySelector(".expandButton");
+      // console.log(this.expandButton);
+  
+      // Using your expandButton reference, update the text on your expandButton to say "expand"
+      this.expandButton.textContent = "expand";
+      // console.log(this.expandButton.textContent);
+      
+      // Set a click handler on the expandButton reference, calling the expandArticle method.
+      this.expandButton.addEventListener('click', () => this.expandArticle());
+    }
+  
+    expandArticle() {
+      // Using our reference to the domElement, toggle a class to expand or hide the article.
+      console.log("The toggle-function of the domElement was triggered due to a click on the expand button.");
+      this.domElement.classList.toggle("article-open");
+      this.expandButton.textContent == 'Collapse'
+          ? this.expandButton.textContent = 'Expand'
+          : this.expandButton.textContent = 'Collapse';
+    }
+  }
+  
+  /* START HERE: 
+  
+  - Select all classes named ".article" and assign that value to the articles variable.  
+  
+  - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+  
+  */
+  
+  // let article = document.querySelector(".article");
+  
+  let articles = document.querySelectorAll(".article");
+  console.log(articles);
+  
+//   articles.forEach( (article) => {
+//     console.log(article);
+//     return new Article(article);
+//   });
+  
+  articles.forEach( article => new Article(article));
 
-//   expandArticle() {
-//     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
-//   }
-// }
 
-// /* START HERE: 
 
-// - Select all classes named ".article" and assign that value to the articles variable.  
 
-// - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
-// */
- 
-// // let articles;
 
-// class Article {
-//   // constructor
-//   constructor(article) {
-//     // panel is the individual DOM panel
-//     this.panelButtons = article.querySelector('.panel-buttons');
-//     this.panelBtnOpen = article.querySelector('.panel-btn-open');
-//     this.panelBtnClose = article.querySelector('.panel-btn-close');
-//     // this.panelContent = article.querySelector('.panel-content');
-    
-//     //this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
-//     this.panelButtons.addEventListener('click', () => this.togglePanel());
-    
-//   }
-//   // methods
-//   togglePanel() {
-//     console.log(this.panelBtnOpen);
-//     this.panelBtnOpen.classList.toggle('hide-btn');
-//     this.panelBtnClose.classList.toggle('hide-btn');
-//     this.panelContent.classList.toggle('toggle-on');
-//   }
-// }
 
-// const articles = document.querySelectorAll('.article');
-//   articles.forEach( article => new Article(article));
